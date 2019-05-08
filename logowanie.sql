6e9d44bdb0d42f6604e19581e28043b2a2249185
P4llix <45855475+P4llix@users.noreply.github.com>
2019-05-08 16:40:02 +0200

    Add files via upload

--- logowanie.sql
+++ logowanie.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Czas generowania: 07 Maj 2019, 16:39
+-- Czas generowania: 08 Maj 2019, 16:39
 -- Wersja serwera: 5.5.39
 -- Wersja PHP: 5.4.31
 
@@ -33,17 +33,23 @@ CREATE TABLE IF NOT EXISTS `logowanie` (
   `Mail` char(40) CHARACTER SET utf8 NOT NULL,
   `dataLogSU` text CHARACTER SET utf8 NOT NULL,
   `dataLogDe` text CHARACTER SET utf8 NOT NULL,
-  `imie` char(30) CHARACTER SET utf8 NOT NULL,
-  `nazwisko` char(30) CHARACTER SET utf8 NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+  `imie` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `nazwisko` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `status` varchar(30) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 --
 -- Zrzut danych tabeli `logowanie`
 --
 
-INSERT INTO `logowanie` (`ID`, `Login`, `Haslo`, `Mail`, `dataLogSU`, `dataLogDe`, `imie`, `nazwisko`) VALUES
-(1, 'admin', 'YWRtaW4=', 'admin.admin@gmail.com', '2019-05-07 16:38:08', '2019-05-07 16:36:33', 'Jan', 'Kowalski'),
-(3, 'user1', 'dXNlcjE=', 'user1@o2.pl', '2019-05-07 16:29:37', '2019-05-07 16:28:07', 'Mateusz', 'Nowak');
+INSERT INTO `logowanie` (`ID`, `Login`, `Haslo`, `Mail`, `dataLogSU`, `dataLogDe`, `imie`, `nazwisko`, `status`) VALUES
+(1, 'admin', 'YWRtaW4=', 'admin@o2.pl', '2019-05-08 15:24:19', '2019-05-07 16:36:33', 'Adam', 'Kowalski', 'Odblokowany'),
+(2, 'administrator', '', 'administracja@wp.pl', '', '', 'Krzysztof', 'zxc', 'Odblokowany'),
+(3, 'user1', 'dXNlcjE=', 'user1.user1@o2.pl', '2019-05-08 14:26:47', '2019-05-08 14:26:39', 'Adrian', 'Polak', 'Odblokowany'),
+(4, 'user2', '', 'user2.user2@wp.pl', '2019-05-08 15:24:07', '2019-05-08 12:44:09', 'PaweÅ‚', 'Donald', 'Zablokowany'),
+(5, 'user3', '', 'user3.user3@onet.pl', '', '', 'mikoÅ‚aj', 'qwerty', 'Zablokowany'),
+(6, 'user4', '', 'user4.user4@onet.pl', '', '', 'Albert', 'asdfg', 'Odblokowany'),
+(7, 'user3', '', 'user3.user3@onet.pl', '', '', 'mikoÅ‚aj', 'qwerty', 'Zablokowany');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -63,7 +69,7 @@ ALTER TABLE `logowanie`
 -- AUTO_INCREMENT dla tabeli `logowanie`
 --
 ALTER TABLE `logowanie`
-MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
